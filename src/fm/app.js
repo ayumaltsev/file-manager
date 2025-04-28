a78a2029290780de99ffd298fcc49ca255034f66
Anton Maltsev <Anton.Maltsev@McKesson.com>
2025-04-28 19:16:24 +0300

    init: start file-manager task

---
+++ src/fm/app.js
@@ -0,0 +1,31 @@
+const parseArgs = () => {
+
+    const args = process.argv.slice(2);
+    const welcomeMessage = 'Welcome to the File Manager, Username!';
+    const errorMessage = 'Please, start the application with only one argument called username!\n'
+        + 'Example: npm run start -- --username=your_username';
+    let userName = 'noname';
+
+    try {
+        if (args.length > 1 || args.length === 0) {
+            throw new Error();
+        }
+
+        const firstArg = args[0].trim().slice(2);
+        const spitedNameValue = firstArg.split('=');
+        if (spitedNameValue[0].trim() === 'username') {
+            userName = spitedNameValue[1].trim();
+        } else {
+            throw new Error();
+        }
+
+    } catch (error) {
+        console.error(errorMessage);
+        process.exit(1);
+    }
+
+    console.log(welcomeMessage.replace('Username', userName));
+    return userName;
+};
+
+const userName = parseArgs();
